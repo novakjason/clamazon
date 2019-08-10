db2c1f29eb80058e2f075bf9b3239e272f280d97
Red Tail <redtail814@gmail.com>
2019-08-10 05:03:14 -0400

    Added purchase(); to update MySQL database based on user's selection

--- clamazonCustomer.js
+++ clamazonCustomer.js
@@ -60,6 +60,7 @@ let chooseDept = () => {
                     viewDairy(answer);
                     break;
                 case "Exit":
+                    console.log("Thank you for shopping at Clamazon!")
                     connection.end();
                     break;
             }
@@ -68,32 +69,33 @@ let chooseDept = () => {
 
 
 //////////  navigation for the user between inventory views
-let navigate = () => {
+let navigate = (res) => {
     inquirer
         .prompt({
             type: "list",
             name: "confirm",
             message: "\n\nPlease choose from the following options:     ",
             choices: [
+                new inquirer.Separator(),
                 "Purchase inventory",
+                "Choose new department",
                 new inquirer.Separator(),
-                "Choose department",
-                new inquirer.Separator(),
-                "Exit\n"
+                "Exit"
             ],
-            pageSize: 7,
-            prefix: ""
+            pageSize: 7
         })
         .then((answer) => {
 
             switch (answer.confirm) {
                 case "Purchase inventory":
-                    purchase();
+                    //////////  calling the purchase function and passing through the mysql database query response
+                    purchase(res);
                     break;
-                case "Choose department":
+                case "Choose new department":
                     chooseDept();
                     break;
                 case "Exit":
+                    console.log("Thank you for shopping at Clamazon!")
                     connection.end();
                     break;
             }
@@ -109,7 +111,7 @@ const table = new Table({
 
 
 //////////  function to fill the new table with data from mysql database
-let newTable = (query, answer) => {
+let newTable = (query, answer, res) => {
 
     //////////  Emptying table contents but keeping the header.
     table.length = 0;
@@ -128,43 +130,110 @@ let newTable = (query, answer) => {
         }
         //////////  displaying newly filled table in the cli
         console.log('\n' + table.toString() + '\n\n\n');
+
+        //////////  calling the navigate function and passing through the mysql database query response
+        navigate(res);
     });
 }
 
 
 //////////  function to view all inventory sorted by department
-let viewAll = (answer) => {
+let viewAll = (answer, res) => {
 
     //////////  SQL SELECT command to query inventory data
     let query = "SELECT * FROM products ORDER BY department_name, item_id";
 
-    newTable(query, answer);
-    navigate();
+    //////////  newTable function eliminates redundancy in view functions
+    newTable(query, answer, res);
 }
 
-let deptQuery = "SELECT * FROM products WHERE department_name = ?";
 
-let viewProduce = (answer) => {
+//////////  one variable to hold mysql query parameters for searching a specific department
+const deptQuery = "SELECT * FROM products WHERE department_name = ?";
+
+
+//////////  function to view produce inventory
+let viewProduce = (answer, res) => {
     let query = deptQuery;
-    newTable(query, answer);
-    navigate();
+    newTable(query, answer, res);
 }
 
 
-let viewBakery = (answer) => {
+//////////  bakery
+let viewBakery = (answer, res) => {
     let query = deptQuery;
-    newTable(query, answer);
-    navigate();
+    newTable(query, answer, res);
 }
 
 
-let viewDairy = (answer) => {
+//////////  you get it yet?
+let viewDairy = (answer, res) => {
     let query = deptQuery;
-    newTable(query, answer);
-    navigate();
+    newTable(query, answer, res);
 }
 
-let purchase = () => {
-    console.log("TESTING purchase();");
-    navigate();
+
+//////////  function to purchase items from inventory using item_id and quanity of stock. passing in mysql query response
+let purchase = (res) => {
+
+    inquirer
+    //////////  asking for ITEM ID
+        .prompt({
+            type: "input",
+            name: "item",
+            message: "Please enter ITEM ID."
+        })
+        .then((answer) => {
+
+            //////////  looping through mysql query response for user input validation
+            for (var i = 0; i < res.length; i++) {
+
+                //////////  validating user's input against each item_id in database
+                if (answer.item == res[i].item_id) {
+
+                    //////////  variable to hold user's input to pass into the next inquirer promise
+                    let itemID = answer.item;
+
+                    //////////  variable to hold subarray of user's input to pass into the next inquirer promise
+                    let j = i;
+
+                    //////////  asking user for the amount of stock they wish to purchase
+                    inquirer
+                        .prompt({
+                            type: "input",
+                            name: "quantity",
+                            message: "Enter the amount you would like to purchase.",
+
+                            //////////  validating user's input to make sure it is a number
+                            validate: (value) => {
+                                if (isNaN(value) == false) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        }).then((answer) => {
+
+                            //////////  checking if there is sufficient stock available for user to purchase
+                            if ((res[j].stock - answer.quantity) >= 0) {
+
+                                //////////  updating the mysql clamazonDB database with the new stock quantity if purchase was successful
+                                connection.query("UPDATE products SET stock='" + (res[j].stock - answer.quantity) + "' WHERE item_id='" + itemID + "'", (err, res) => {
+                                    if (err) throw err;
+
+                                    console.log("Your purchase has been made.");
+
+                                    //////////  restarting application if purchase was successful
+                                    chooseDept();
+                                })
+                            } else {
+                                console.log("Insufficient stock available.  Please select a smaller quantity.");
+
+                                //////////  restarting purchase function if stock is not available for user
+                                purchase(res);
+                            }
+                        })
+                }
+            }
+        });
 }
